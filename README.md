916271faa089693137c706bb6f15d52e43f09023
Rahul Bawa <Rahul Bawa rahul.bawa@ericsson.com>
2019-10-11 02:09:26 +0530

    Update the README.md with instructions - Code refactor

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# gerrit-api
-A command line way of interacting with gerrit's api
+### Gerrit-Api
+This project wraps the gerrit's api using a bash script.
+
+### Usage
+Export the following environment variables
+```
+export GERRIT_USERNAME="johnedoe"
+export GERRIT_HTTP_PASSWORD="password"
+export GERRIT_SERVER_HOSTNAME="foo.org.com"
+```
+
+Import the methods
+```
+source ./script.sh
+```
+
+### Methods supported as of now
+
+#### set review
+```
+name: set_review_for_latest_revision 590443 2
+args: change_id -1/1/2/-2/0
+```
+#### Get current revision
+```
+name: __get_revision_id
+args: change_id
+```
+
+#### Submit the change
+```
+name: submit_change
+args: change_id
+```
+
+### Example method invocation
+
+#### set review
+```
+set_review_for_latest_revision 590443 2
+```
+
+#### Get the latest revision
+```
+rev_id=$(__get_revision_id 590443)
+```
+
+#### submit the change
+```
+submit_change 590443
+```
--- script.sh
+++ script.sh
@@ -1,77 +1,63 @@
 #!/bin/bash
 
 # internal utility methods
-local SIGNUM="erahbaw"
-local HTTP_PASSWORD="ufBgY33ZH0TWWNL6HBNrcVAGqk4UCqz1OyKk3Lvt5A"
-local BASIC_AUTH="${SIGNUM}:${HTTP_PASSWORD}"
-local BASE_URL="https://gerrit.epk.ericsson.se/a"
+local GERRIT_USERNAME="${GERRIT_USERNAME}"
+local GERRIT_HTTP_PASSWORD="${GERRIT_HTTP_PASSWORD}"
+local BASIC_AUTH="${GERRIT_USERNAME}:${GERRIT_HTTP_PASSWORD}"
+local GERRIT_SERVER_HOSTNAME="https://${GERRIT_SERVER_HOSTNAME}/a"
 
-function __prepare_digest_request_str()
-{
-local URL="$1"
-local TYPE="$2"
-local PAYLOAD="$3"
-local HEADER=''
-case "$2" in
-        "POST"|"post"|"p")
-                if [[ -z "$PAYLOAD" ]];then 
-			echo "Expecting a json payload as third argument"
-			exit 1
-		else
-                	HEADER='Content-Type: application/json'
-                	echo "curl -s --digest -u ${BASIC_AUTH} -X POST \"${BASE_URL}${URL}\" -d \"${PAYLOAD}\" -H \"${HEADER}\""
-		fi
-         	;;
-        "GET"|"get"|"g")
-		echo "curl -s --digest -u ${BASIC_AUTH} -X GET \"${BASE_URL}${URL}\""
-                ;;
-        *)
-                echo "Error: Unsupported request type found. Only Get/Post request supported."
-                ;;
-esac
+function __prepare_digest_request_str() {
+  local URL="$1"
+  local TYPE="$2"
+  local PAYLOAD="$3"
+  local HEADER=''
+  case "$2" in
+  "POST" | "post" | "p")
+    if [[ -z "$PAYLOAD" ]]; then
+      echo "Expecting a json payload as third argument"
+      exit 1
+    else
+      HEADER='Content-Type: application/json'
+      echo "curl -s --digest -u ${BASIC_AUTH} -X POST \"${GERRIT_SERVER_HOSTNAME}${URL}\" -d \"${PAYLOAD}\" -H \"${HEADER}\""
+    fi
+    ;;
+  "GET" | "get" | "g")
+    echo "curl -s --digest -u ${BASIC_AUTH} -X GET \"${GERRIT_SERVER_HOSTNAME}${URL}\""
+    ;;
+  *)
+    echo "Error: Unsupported request type found. Only Get/Post request supported."
+    ;;
+  esac
 }
 
-function __get_revision_id()
-{
-local CHANGE_ID=$1
-local URL="/changes/${CHANGE_ID}/?o=CURRENT_REVISION"
-local COMMAND_STR=$(__prepare_digest_request_str "${URL}" "g")
-local REVISION_ID=$(eval ${COMMAND_STR} | grep -e "current_revision" | awk '{print $2}' | grep -o "[a-z0-9]\+")
-[[ $? -eq 0 ]] && echo "${REVISION_ID}" || return 1
+function __get_revision_id() {
+  local CHANGE_ID=$1
+  local URL="/changes/${CHANGE_ID}/?o=CURRENT_REVISION"
+  local COMMAND_STR=$(__prepare_digest_request_str "${URL}" "g")
+  local REVISION_ID=$(eval ${COMMAND_STR} | grep -e "current_revision" | awk '{print $2}' | grep -o "[a-z0-9]\+")
+  [[ $? -eq 0 ]] && echo "${REVISION_ID}" || return 1
 }
 
 # utility methods end here.
 
-function set_review_for_latest_revision()
-{
-echo "Giving $2 for the change id: $1"
-local CHANGE_ID=$1
-local REVIEW_RATING="$2"
-local PAYLOAD='{"labels": {"Code-Review": '"${REVIEW_RATING}"'}}'
-local REVISION_ID=$(__get_revision_id "${CHANGE_ID}")
-local URL="/changes/${CHANGE_ID}/revisions/${REVISION_ID}/review"
-# execute the command string
-local COMMAND_STR=$(__prepare_digest_request_str "${URL}" "p" "${PAYLOAD}")
-eval "${COMMAND_STR}"
-[[ $? -eq 0 ]] && echo "Changeset reviewed successfully" || echo "Failure: could not set the review"
+function set_review_for_latest_revision() {
+  echo "Giving $2 for the change id: $1"
+  local CHANGE_ID=$1
+  local REVIEW_RATING="$2"
+  local PAYLOAD='{"labels": {"Code-Review": '"${REVIEW_RATING}"'}}'
+  local REVISION_ID=$(__get_revision_id "${CHANGE_ID}")
+  local URL="/changes/${CHANGE_ID}/revisions/${REVISION_ID}/review"
+  # execute the command string
+  local COMMAND_STR=$(__prepare_digest_request_str "${URL}" "p" "${PAYLOAD}")
+  eval "${COMMAND_STR}"
+  [[ $? -eq 0 ]] && echo "Changeset reviewed successfully" || echo "Failure: could not set the review"
 }
 
-function submit_change()
-{
- echo "Submitting the change $1"
- local CHANGE_ID=$1
- local URL="/changes/${CHANGE_ID}/submit"
- local COMMAND_STR=$(__prepare_digest_request_str "${URL}" "p" '{"wait_for_merge": true}')
- eval "${COMMAND_STR}"
- [[ $? -eq 0 ]] && echo "Change $1 submitted successfully" || echo "Failure: change could not be submitted"
+function submit_change() {
+  echo "Submitting the change $1"
+  local CHANGE_ID=$1
+  local URL="/changes/${CHANGE_ID}/submit"
+  local COMMAND_STR=$(__prepare_digest_request_str "${URL}" "p" '{"wait_for_merge": true}')
+  eval "${COMMAND_STR}"
+  [[ $? -eq 0 ]] && echo "Change $1 submitted successfully" || echo "Failure: change could not be submitted"
 }
-
-# Examples calls
-# set review
-#set_review_for_latest_revision 590443 2
-
-# get current revision change
-#rev_id=$(__get_revision_id 590443)
-
-# submit the change
-#submit_change 590443
