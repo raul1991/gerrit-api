4d995de99fae758c1898407b07e98edaf587d8e8
Rahul Bawa <Rahul Bawa rahul.bawa@ericsson.com>
2019-10-11 01:49:09 +0530

    Add more methods for gerrit api
    - Submit a patch to gerrit (if permitted)
    - Get revision id (helper)
    - Review a change's current revision

--- script.sh
+++ script.sh
@@ -1,37 +1,58 @@
 #!/bin/bash
 
-function send_digest_request()
+# internal utility methods
+local SIGNUM="erahbaw"
+local HTTP_PASSWORD="ufBgY33ZH0TWWNL6HBNrcVAGqk4UCqz1OyKk3Lvt5A"
+local BASIC_AUTH="${SIGNUM}:${HTTP_PASSWORD}"
+local BASE_URL="https://gerrit.epk.ericsson.se/a"
+
+function __prepare_digest_request_str()
 {
-  
+local URL="$1"
+local TYPE="$2"
+local PAYLOAD="$3"
+local HEADER=''
+case "$2" in
+        "POST"|"post"|"p")
+                if [[ -z "$PAYLOAD" ]];then 
+			echo "Expecting a json payload as third argument"
+			exit 1
+		else
+                	HEADER='Content-Type: application/json'
+                	echo "curl -s --digest -u ${BASIC_AUTH} -X POST \"${BASE_URL}${URL}\" -d \"${PAYLOAD}\" -H \"${HEADER}\""
+		fi
+         	;;
+        "GET"|"get"|"g")
+		echo "curl -s --digest -u ${BASIC_AUTH} -X GET \"${BASE_URL}${URL}\""
+                ;;
+        *)
+                echo "Error: Unsupported request type found. Only Get/Post request supported."
+                ;;
+esac
 }
 
-function get_revision_id()
+function __get_revision_id()
 {
-echo "Fetching the latest revision id for the change id $2"
-local SIGNUM=$1
-local CHANGE_ID=$2
-local HTTP_PASSWORD=$3
-local REVIEW_RATING=$4
-local PAYLOAD='{"message": "Submitting the change using gerrit-api", labels: {"Code-Review": '"${REVIEW_RATING}"'}}'
-local BASE_URL="https://gerrit.epk.ericsson.se/a/changes"
-local BASIC_AUTH="${SIGNUM}:${HTTP_PASSWORD}"
-local REVISION_ID=$(curl -s --digest -u "${BASIC_AUTH}" "${BASE_URL}/${CHANGE_ID}/?o=CURRENT_REVISION" | grep -e "current_revision" | awk '{print $2}' | grep -o "[a-z0-9]\+")
-curl -s --digest -u "${BASIC_AUTH}" "${BASE_URL}/${CHANGE_ID}/revisions/${REVISION_ID}/review" -d '"$PAYLOAD"' -H 'Content-Type: application/json' > /dev/null
-[[ $? -eq 0 ]] && echo "Changeset reviewed successfully" || echo "Failure: could not set the review"
+local CHANGE_ID=$1
+local URL="/changes/${CHANGE_ID}/?o=CURRENT_REVISION"
+local COMMAND_STR=$(__prepare_digest_request_str "${URL}" "g")
+local REVISION_ID=$(eval ${COMMAND_STR} | grep -e "current_revision" | awk '{print $2}' | grep -o "[a-z0-9]\+")
+[[ $? -eq 0 ]] && echo "${REVISION_ID}" || return 1
 }
 
-function set_review()
+# utility methods end here.
+
+function set_review_for_latest_revision()
 {
-echo "Setting the review for $2"
-local SIGNUM=$1
-local CHANGE_ID=$2
-local HTTP_PASSWORD=$3
-local REVIEW_RATING=$4
-local PAYLOAD='{"message": "Submitting the change using gerrit-api", labels: {"Code-Review": '"${REVIEW_RATING}"'}}'
-local BASE_URL="https://gerrit.epk.ericsson.se/a/changes"
-local BASIC_AUTH="${SIGNUM}:${HTTP_PASSWORD}"
-local REVISION_ID=$(curl -s --digest -u "${BASIC_AUTH}" "${BASE_URL}/${CHANGE_ID}/?o=CURRENT_REVISION" | grep -e "current_revision" | awk '{print $2}' | grep -o "[a-z0-9]\+")
-curl -s --digest -u "${BASIC_AUTH}" "${BASE_URL}/${CHANGE_ID}/revisions/${REVISION_ID}/review" -d '"$PAYLOAD"' -H 'Content-Type: application/json' > /dev/null
+echo "Giving $2 for the change id: $1"
+local CHANGE_ID=$1
+local REVIEW_RATING="$2"
+local PAYLOAD='{"labels": {"Code-Review": '"${REVIEW_RATING}"'}}'
+local REVISION_ID=$(__get_revision_id "${CHANGE_ID}")
+local URL="/changes/${CHANGE_ID}/revisions/${REVISION_ID}/review"
+# execute the command string
+local COMMAND_STR=$(__prepare_digest_request_str "${URL}" "p" "${PAYLOAD}")
+eval "${COMMAND_STR}"
 [[ $? -eq 0 ]] && echo "Changeset reviewed successfully" || echo "Failure: could not set the review"
 }
 
@@ -39,6 +60,18 @@ function submit_change()
 {
  echo "Submitting the change $1"
  local CHANGE_ID=$1
-
+ local URL="/changes/${CHANGE_ID}/submit"
+ local COMMAND_STR=$(__prepare_digest_request_str "${URL}" "p" '{"wait_for_merge": true}')
+ eval "${COMMAND_STR}"
+ [[ $? -eq 0 ]] && echo "Change $1 submitted successfully" || echo "Failure: change could not be submitted"
 }
-set_review erahbaw 590443 ufBgY33ZH0TWWNL6HBNrcVAGqk4UCqz1OyKk3Lvt5A -1
+
+# Examples calls
+# set review
+#set_review_for_latest_revision 590443 2
+
+# get current revision change
+#rev_id=$(__get_revision_id 590443)
+
+# submit the change
+#submit_change 590443
